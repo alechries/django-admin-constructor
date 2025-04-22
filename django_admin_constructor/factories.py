58ce600127ccc07ff3a22ca407a6e387b1c04e04
Alexander Wiese <51765464+alechries@users.noreply.github.com>
2025-04-22 10:07:08 +0200

    Update factories.py

--- django_admin_constructor/factories.py
+++ django_admin_constructor/factories.py
@@ -7,21 +7,12 @@ class ModelFactory:
 
     @staticmethod
     def get_model(name: str):
-        """
-        Factory-Methode zum Erhalten von Modellen basierend auf dem Namen.
-        Es wird nur eine Instanz jedes Modells erstellt.
-        """
         if name not in ModelFactory._instances:
             ModelFactory._instances[name] = ModelFactory._create_model(name)
         return ModelFactory._instances[name]
 
     @staticmethod
     def _create_model(name: str):
-        """
-        Dynamisches Erstellen eines Modells zur Laufzeit.
-        Hier kannst du weitere Feldtypen dynamisch hinzufügen.
-        """
-        # Die Felder, die das dynamische Modell haben soll.
         fields = {
             'name': models.CharField(max_length=100),
             'price': models.DecimalField(max_digits=10, decimal_places=2),
@@ -33,8 +24,5 @@ class ModelFactory:
         return new_model_class
 
 
-# Beispiel für die Verwendung:
 # product_model = ModelFactory.get_model('Product')
-
-# Jetzt kannst du mit dem dynamischen Modell arbeiten:
 # product = product_model.objects.create(name="Test Produkt", price=19.99, description="Testbeschreibung")
